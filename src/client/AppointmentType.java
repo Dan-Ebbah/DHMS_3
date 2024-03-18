f3ea2c59985b6625d977524d5afe080b48e45aea
Daniel Dan-Ebbah <danielebbah@yahoo.com>
2024-03-18 08:13:17 -0400

    created classes from wsimport

---
+++ src/client/AppointmentType.java
@@ -0,0 +1,54 @@
+
+package client;
+
+
+import jakarta.xml.bind.annotation.XmlEnum;
+import jakarta.xml.bind.annotation.XmlEnumValue;
+import jakarta.xml.bind.annotation.XmlType;
+
+/**
+ * <p>Java class for appointmentType.
+ * 
+ * <p>The following schema fragment specifies the expected content contained within this class.
+ * <p>
+ * <pre>
+ * &lt;simpleType name="appointmentType">
+ *   &lt;restriction base="{http://www.w3.org/2001/XMLSchema}string">
+ *     &lt;enumeration value="Physician"/>
+ *     &lt;enumeration value="Surgeon"/>
+ *     &lt;enumeration value="Dental"/>
+ *   &lt;/restriction>
+ * &lt;/simpleType>
+ * </pre>
+ * 
+ */
+@XmlType(name = "appointmentType")
+@XmlEnum
+public enum AppointmentType {
+
+    @XmlEnumValue("Physician")
+    PHYSICIAN("Physician"),
+    @XmlEnumValue("Surgeon")
+    SURGEON("Surgeon"),
+    @XmlEnumValue("Dental")
+    DENTAL("Dental");
+    private final String value;
+
+    AppointmentType(String v) {
+        value = v;
+    }
+
+    public String value() {
+        return value;
+    }
+
+    public static AppointmentType fromValue(String v) {
+        for (AppointmentType c: AppointmentType.values()) {
+            if (c.value.equals(v)) {
+                return c;
+            }
+        }
+        throw new IllegalArgumentException(v);
+    }
+
+}
---
+++ src/client/UdpServerInfo.java
@@ -0,0 +1,32 @@
+
+package client;
+
+
+import jakarta.xml.bind.annotation.XmlAccessType;
+import jakarta.xml.bind.annotation.XmlAccessorType;
+import jakarta.xml.bind.annotation.XmlType;
+
+/**
+ * <p>Java class for udpServerInfo complex type.
+ * 
+ * <p>The following schema fragment specifies the expected content contained within this class.
+ * 
+ * <pre>
+ * &lt;complexType name="udpServerInfo">
+ *   &lt;complexContent>
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *       &lt;sequence>
+ *       &lt;/sequence>
+ *     &lt;/restriction>
+ *   &lt;/complexContent>
+ * &lt;/complexType>
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "udpServerInfo")
+public class UdpServerInfo {
+
+
+}
